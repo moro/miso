1f046373df541b7db89e42363ef2369ca162b994
moro <moronatural@gmail.com>
2009-05-25 13:56:54 +0900

    extra web-app. steps.

---
+++ rails_generators/miso/templates/web_extra_ja_steps.rb
@@ -0,0 +1,25 @@
+Given /^言語は"([^\"]*)"$/ do |lang|
+  header("ACCEPT_LANGUAGE", lang)
+end
+
+When /^"([^\"]*)"としてファイル"([^\"]*)"をContent\-Type"([^\"]*)"として添付する$/ do |field, path, content_type|
+  attach_file(field, path, content_type)
+end
+
+When /"([^\"]*)"中の"([^\"]*)"リンクをクリックする$/ do |element, label|
+  click_link_within(element, label)
+end
+
+When /^テーブル"([^\"]*)"の"([^\"]*)"行目の"([^\"]*)"リンクをクリックする$/ do |table, n, label|
+  selector = "table.#{cls} tbody tr:nth(#{nth})"
+  click_link_within(selector, link)
+end
+
+When /^デバッグ.*$/ do
+  save_and_open_page
+end
+
+Then /^テキストフィールドに"([^\"]*)"と表示されていること$/ do |arg1|
+  response.should have_tag("input[type=text][value=#{text}]")
+end
+
